aa227e68d536979e2794b72d4f262f29d436cad3
nsetty <nsett7388@gmail.com>
2024-02-29 19:30:29 -0500

    added renderLiscenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = '';
+  if(license === 'MIT') {
+      badge = '![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)'
+  } else if (license === 'GPL v3.0') {
+      badge = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)'
+  } else if (license === 'Apache 2.0') {
+    badge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)'
+  }else {
+    badge = ""
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
