0da01a32f2201f10d29e300e557877d2c5ac3fe1
nsetty <nsett7388@gmail.com>
2024-02-29 19:42:58 -0500

    fixed index file

--- index.js
+++ index.js
@@ -9,119 +9,47 @@ const questions = [
         type: 'input',
         name: 'title',
         message: 'What is the title of your project?',
-        validate: titleInput => {
-            if (titleInput) {
-                return true;
-            } else {
-                console.log('Please enter a title for your project');
-                return false;
-            }
-        }
     },
     {
         type: 'list',
         name: 'license',
         message: 'What liscenese did you use for this project?',
         choices: ['None', 'MIT', 'GPL v3.0', 'Apache 2.0'],
-        alidate: licenseInput => {
-            if (licenseInput) {
-                return true;
-            } else {
-                console.log('Please choose a liscense for your project');
-                return false;
-            }
-        }
     },
     {
         type: 'input',
         name: 'description',
         message: 'Provide a description for your project.',
-        validate: descriptionInput => {
-            if (descriptionInput) {
-                return true;
-            } else {
-                console.log('Please provide a description for your project');
-                return false;
-            }
-        }
     },
     {
         type: 'input',
         name: 'installation',
         message: 'What installation steps does your project require?',
-        validate: installationInput => {
-            if (installationInput) {
-                return true;
-            } else {
-                console.log('Please enter installation steps for your project');
-                return false;
-            }
-        }
     },
     {
         type: 'input',
         name: 'usage',
         message: 'What is the use for your project?',
-        validate: usageInput => {
-            if (usageInput) {
-                return true;
-            } else {
-                console.log('Please provide a use for your project');
-                return false;
-            }
-        }
     },
     {
         type: 'input',
         name: 'contributions',
         message: 'What guidelines must others follow in order to contribute?',
-        validate: contributionsInput => {
-            if (contributionsInput) {
-                return true;
-            } else {
-                console.log('Please enter contribution guidelines');
-                return false;
-            }
-        }
     },
     {
         type: 'input',
         name: 'tests',
         message: 'How do you test this project?',
-        validate: testingInput => {
-            if (testingInput) {
-                return true;
-            } else {
-                console.log('Please explain how to test this project');
-                return false;
-            }
-        }
     },
     {
         type: 'input',
         name: 'gitHib',
         message: 'What is your Github username?',
-        validate: gitHiubInput => {
-            if (gitHiubInput) {
-                return true;
-            } else {
-                console.log('Please provide your github username');
-                return false;
-            }
-        }
     },
     {
         type: 'input',
         name: 'email',
         message: 'What is your email?',
-        validate: emailInput => {
-            if (emailInput) {
-                return true;
-            } else {
-                console.log('Please provide an email');
-                return false;
-            }
-        }
     }
 ];
 
@@ -137,13 +65,14 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-        .prompt(questions)
+    inquirer.prompt(questions)
         .then((answers) => {
-            const markDownContent = generateMarkdown(answers);
-            fs.writeFile('README.md', markDownContent, (err) =>
-                err ? console.log(err) : console.log('The README.md file has been updated.')
-            )
+            const markdownContent = generateMarkdown(answers);
+            const fileName = 'PRO-README-GEN.md';
+            writeToFile(fileName, markdownContent);
+        })
+        .catch((error) => {
+            console.error('An error occurred during the initialization:', error);
         });
 }
 
