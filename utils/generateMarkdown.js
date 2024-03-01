77004336531883e3db9b374db2a32bdec66411e7
nsetty <nsett7388@gmail.com>
2024-02-29 19:37:30 -0500

    added renderLiscenseSection file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -32,7 +32,16 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = ''
+  if(license === 'None') {
+    licenseSection = ''
+  } else {
+    licenseSection =
+    `License: ${license} `
+  }
+  return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
